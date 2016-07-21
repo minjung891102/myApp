0f1030237605412fc562ec6baa05753491a5170c
minjung891102 <cjsdb1126@naver.com>
2016-07-21 15:11:53 +0900

    time function error, reset

--- models/Post.js
+++ models/Post.js
@@ -11,17 +11,4 @@ var postSchema = mongoose.Schema({
 });
 var Post = mongoose.model('post', postSchema);
 
-postSchema.methods.getCreatedDate = function () {
-  var date = this.createdAt;
-  return date.getFullYear() + "-" + get2digits(date.getMonth()+1)+ "-" + get2digits(date.getDate());
-};
-
-postSchema.methods.getCreatedTime = function () {
-  var date = this.createdAt;
-  return get2digits(date.getHours()) + ":" + get2digits(date.getMinutes())+ ":" + get2digits(date.getSeconds());
-};
-function get2digits(num){
-  return ("0" + num).slice(-2);
-}
-
 module.exports = Post;
