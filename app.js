4da08612b9633314604482d18451bc5f9a91fe5e
minjung891102 <cjsdb1126@naver.com>
2016-07-22 17:00:54 +0900

    counter >> app,js myapp update

--- app.js
+++ app.js
@@ -70,22 +70,22 @@ app.listen(port, function() {
 
 
 
-function countVisitors(req, res, next) {
-  if(!req.cookies.count && req.cookies['connect.sid']) {
-    res.cookie('count',"", {maxAge:3600000, httpOnly:true});
+function countVisitors(req,res,next){
+  if(!req.cookies.count&&req.cookies['connect.sid']){
+    res.cookie('count', "", { maxAge: 3600000, httpOnly: true });
     var now = new Date();
-    var date = now.getFullYear() + "/" + now.getMonth + "/" + now.getDate();
-    if(date != req.cookies.coountDate) {
-      res.cookie('countDate', date, {maxAge:86400000, httpOnly:true});
+    var date = now.getFullYear() +"/"+ now.getMonth() +"/"+ now.getDate();
+    if(date != req.cookies.countDate){
+      res.cookie('countDate', date, { maxAge: 86400000, httpOnly: true });
 
-      var Counter = require('./model/Coounter');
-      Couonter.findOne({name:"visitors"}, function(err,coounter) {
+      var Counter = require('./models/Counter');
+      Counter.findOne({name:"vistors"}, function (err,counter) {
         if(err) return next();
-        if(counter===null) {
-          Counter.create({name:"visitors", totalCount:1, todayCount:1, date:date});
+        if(counter===null){
+          Counter.create({name:"vistors",totalCount:1,todayCount:1,date:date});
         } else {
           counter.totalCount++;
-          if(counter.date == date) {
+          if(counter.date == date){
             counter.todayCount++;
           } else {
             counter.todayCount = 1;
